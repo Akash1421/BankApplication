e3ef45146d355f2cb439b4b0b6b22775e48da96d
Akash1421 <akashreddy1421@gmail.com>
2025-08-25 21:51:11 +0530

    added headers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),importProvidersFrom(FlexLayoutModule,MatMenuModule,MatButtonModule)]
 };
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../Header-components/menu/menu.component';
+import { MatMenu, MatMenuModule } from "@angular/material/menu";
+import { MatButton, MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatMenu,MatMenuModule,MatButton],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+PendingRequest: any;
+CustomerDetails: any;
 
 }
